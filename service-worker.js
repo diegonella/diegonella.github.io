b52675dd7c1ac892e30f2ed25ec89fe6495536d5
Diego G <diegonella@gmail.com>
2024-07-11 10:08:25 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+<script>
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/service-worker.js')
+                .then(registration => {
+                    console.log('Service Worker registrado con éxito:', registration);
+                })
+                .catch(error => {
+                    console.error('Error al registrar Service Worker:', error);
+                });
+        });
+    }
+</script>
